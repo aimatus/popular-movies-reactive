3f898f2239efb6e87c183c9bef3bffb84e16e9b3
Abraham Matus <aimatusco@gmail.com>
2019-04-10 16:37:35 -0500

    Implements RxAndroid Observable for API call

--- app/src/main/java/com/aimatus/popularmoviesreactive/MainActivity.kt
+++ app/src/main/java/com/aimatus/popularmoviesreactive/MainActivity.kt
@@ -2,14 +2,26 @@ package com.aimatus.popularmoviesreactive
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = this::class.java.canonicalName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Thread(Runnable {
-            println(NetworkUtils.getPopularMovies())
-        }).start()
+        getPopularMovies()
+    }
+
+    private fun getPopularMovies() {
+        Observable.fromCallable { NetworkUtils.getPopularMovies() }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext { popularMoviesResponse -> Log.i(tag, popularMoviesResponse) }
+            .subscribe()
     }
 }
